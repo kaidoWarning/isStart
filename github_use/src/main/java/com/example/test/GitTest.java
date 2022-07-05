28d7156b35cd3244a46033cbc4ee8747cb3daf1f
example <2423751519@qq.com>
2022-07-05 22:30:28 +0800

    test github

--- github_use/src/main/java/com/example/test/GitTest.java
+++ github_use/src/main/java/com/example/test/GitTest.java
@@ -4,7 +4,15 @@ package com.example.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git init");
-        
-        int i =111;
+
+        System.out.println("push test");
+
+        System.out.println("push by ssh");
+
+        System.out.println("push by ssh");
+
+        //something has happened
+
+
     }
 }
