3555b9a9dad8e3ad9db880a32bc50e386af764d1
example <2423751519@qq.com>
2022-07-05 11:53:23 +0800

    yes Initial commit

---
+++ github_use/src/main/java/com/example/GithubUseApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUseApplication.class, args);
+	}
+
+}
